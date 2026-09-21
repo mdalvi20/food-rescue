e19eb31864e33027456f5a2b7c50ea28a375b2f8
MD ALVI <11mdalvi07836@gmail.com>
2026-09-22 00:04:52 +0600

    feat: add UserRepository interface with email queries

---
+++ backend/src/main/java/com/foodrescue/features/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.foodrescue.features.user.repository;
+
+import com.foodrescue.features.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
